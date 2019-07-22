8a8a237fd89861f1950366b9c7a8542c1ac848a3
Clément <le.mikmac@gmail.com>
2019-07-22 14:03:56 +0100

    Fix js server

--- js/server.js
+++ js/server.js
@@ -14,7 +14,8 @@ const evalAi = code => {
 
 const handle = async buffer => {
   const context = JSON.parse(buffer)
-  context.rand = rand(String(context.seed))
+  let seed = Number(context.seed)
+  context.rand = () => (2**32-1 & (seed = Math.imul(16807, seed))) / 2**32
   const { buildContext, ai } = evalAi(await fs.promises.readFile(aiFile, 'utf-8'))
 
   return { context: { ...context, ...buildContext(context) }, ai }
@@ -28,31 +29,19 @@ wss.on('connection', logErr(async ws => {
   const { ai, context } = await new Promise((s, r) =>
     ws.once('message', buffer => s(handle(buffer))))
 
-  ws.send(new TextEncoder().encode(context.name))
+  const { name, count, index } = context
+  ws.send(new TextEncoder().encode(name))
   ws.on('message', async buffer => {
-    if (!context) {
-      context = JSON.parse(buffer)
-      let seed = Number(context.seed)
-      context.rand = () => (2**32-1 & (seed = Math.imul(16807, seed))) / 2**32
-      buildContext(context)
-      ws.send(new TextEncoder().encode(context.name))
-      return
-    }
     let i = -1
-    const players = Array(context.count).fill().map(() => ({}))
-    while (++i < context.count) {
-      const x = i * 2
-      const y = x + 1
-
-      if (i == context.index) {
-        players[0].x = buffer[x]
-        players[0].y = buffer[y]
-      } else if (i > context.index) {
-        players[i].x = buffer[x]
-        players[i].y = buffer[y]
+    const players = Array(count)
+    while (++i < count) {
+      const player = {x: buffer[i*2], y: buffer[i*2 + 1] }
+      if (i == index) {
+        players[0] = player
+      } else if (i > index) {
+        players[i] = player
       } else {
-        players[i+1].x = buffer[x]
-        players[i+1].y = buffer[y]
+        players[i+1] = player
       }
     }
 
